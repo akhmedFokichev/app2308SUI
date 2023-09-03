01a640d9a9a1df9ea59edf3469c0b7cca786d812
Akhmed <f89034909970@gmail.com>
2023-09-03 08:42:32 +0300

    init project

---
+++ App2308SUI/App2308SUI/App2308SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  App2308SUIApp.swift
+//  App2308SUI
+//
+//  Created by Azapsh on 03.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct App2308SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
